b7412bf78367a8307c0cae9bf2a89a363914b524
Concourse <nobody@concourse.ci>
2016-08-30 10:46:40 +0100

    Added default project

---
+++ hello-spring-boot/src/main/java/io/spring/pivotal/HelloSpringBootApplication.java
@@ -0,0 +1,12 @@
+package io.spring.pivotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBootApplication.class, args);
+	}
+}
---
+++ hello-spring-boot/src/test/java/io/spring/pivotal/HelloSpringBootApplicationTests.java
@@ -0,0 +1,18 @@
+package io.spring.pivotal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HelloSpringBootApplication.class)
+@WebAppConfiguration
+public class HelloSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
